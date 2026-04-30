fd7e38f5d88a9f4a4a253fc982bc4f186178ba5f
guwisu <kolyabelykov@mail.ru>
2026-05-01 00:43:33 +0400

    deleted the extra space

--- src/api/auth.py
+++ src/api/auth.py
@@ -19,6 +19,7 @@ from src.services.auth import AuthService
 
 router = APIRouter(prefix="/auth", tags=["Authorization and authentication"])
 
+
 @router.post("/register")
 async def register_user(
     db: DBDep,
